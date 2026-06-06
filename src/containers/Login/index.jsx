2f1f19b05ae9f4a132ace609228ae4cd7dbcdfe0
Lucaslonski-code <lucaslonski890@gmail.com>
2026-06-06 18:49:01 -0300

    login screen structures

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,9 +1,33 @@
 
+import { Container, LeftContainer, RightContainer, Title, Form, InputContainer, Link, Button } from "./styles";
+
+import Logo from '../../assets/Login/Logo.png';
+
 export function Login() { 
 
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Container>
+            <LeftContainer>
+                <img src={Logo} alt="LogoDevBurguer" />
+            </LeftContainer>
+            <RightContainer>
+                <Title>
+                    Olá, seja bem-vindo ao <span>DevBurguer</span>! Acesse com seu <span>Login e senha.</span>
+                </Title>
+                <Form>
+                    <InputContainer>
+                    <label>Email</label>
+                    <input type="email" placeholder="Digite seu email" />
+                    </InputContainer>
+                    <InputContainer>
+                    <label>Senha</label>
+                    <input type="password" placeholder="Digite sua senha" />
+                    </InputContainer>
+                    <Link>Esqueci minha senha</Link>
+                    <Button>Entrar</Button>
+                </Form>
+                <Link>Não tem uma conta? Cadastre-se</Link>
+            </RightContainer>
+        </Container>
     );
 }
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+
+
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+`;
+
+export const LeftContainer = styled.div`
+    display: flex;
+    width: 50%;
+    height: 100%;
+`;
+
+export const RightContainer = styled.div`
+    display: flex;
+    width: 50%;
+    height: 100%;
+`;
+
+export const Title = styled.h2``;
+
+export const Form = styled.form``;
+
+export const InputContainer = styled.div``;
+
+export const Span = styled.span``;
+
+export const Link = styled.a``;
+
+export const Button = styled.button``;
